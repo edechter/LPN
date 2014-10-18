27f834bbf867df728d4809b4ce8c3bb815d9b8bf
EC2 Default User <ec2-user@ip-10-51-145-55.ec2.internal>
2014-10-18 04:20:04 +0000

    More files from Ec2

--- experiments/prism/run_parallel.sh
+++ experiments/prism/run_parallel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NumExamples=(100 200 300 400 500 600 700 800 900 1000);
+NumExamples=(100 300 500 700 900 1100 1300 1500 1700 1900 2100);
 NumReps=10;
-NumPred=(1 2 3);
-SaveDir="data/"
+NumPred=(1 2 3 4);
+SaveDir="data/overnight"
 args=()
 
 for nEx in ${NumExamples[*]}
@@ -19,7 +19,7 @@ do
     done
 done
 
-CMD="parallel -n 3 --delay 2 upprism srs {} $SaveDir &> logs/log_{1}_{2}_{3}.log ::: ${args[*]}"
+CMD="parallel -n 3 --delay 2 upprism srs {} $SaveDir &> logs/overnight/log_{1}_{2}_{3}.log ::: ${args[*]}"
 echo $CMD
 `$CMD`
 
