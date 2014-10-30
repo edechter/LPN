fa6ae0b9dce68768c1e694d1055b01d6f8dabba0
Eyal Dechter <eyaldechter@gmail.com>
2014-10-30 16:15:23 -0400

    Various changes.

--- experiments/prism/onlineVBEM.pl
+++ experiments/prism/onlineVBEM.pl
@@ -1,15 +1,21 @@
 
-ovbem(NIter, BatchSize, DataSet, Rho0) :- 
-    ovbem(NIter, BatchSize, DataSet, Rho0, 1). 
+ovbem(NIter, BatchSize, DataSet, Eps) :- 
+    ovbem(NIter, BatchSize, DataSet, 1, Eps). 
 
-ovbem(NIter, BatchSize, DataSet, Rho0, Iter) :- Iter > NIter, !.
-ovbem(NIter, BatchSize, DataSet, Rho0, Iter) :- 
-    StepSize is (Rho0), 
-    ovbem1(DataSet, BatchSize, StepSize), 
-    Iter1 is Iter+1, 
-    ovbem(NIter, BatchSize, DataSet, Rho0, Iter1).
-    
-    
+ovbem(NIter, BatchSize, DataSet, Iter, Eps) :- Iter > NIter, !.
+ovbem(NIter, BatchSize, DataSet, Iter, Eps) :- 
+    Tau0 is 0,
+    Kappa is 0.8,
+    StepSize is (Tau0 + Iter) ** (-Kappa), 
+    ovbem1(DataSet, BatchSize, StepSize),
+    save_ovbem_alphas(Iter),
+    get_delta_ovbem_alphas(Iter, Delta),
+    write('Iter: '), write(Iter), nl,
+    write('StepSize: '), write(StepSize), nl,
+    write('Normed Delta Alpha: '), write(Delta), nl,
+    ((Iter>1, Delta < Eps) -> true; 
+     (Iter1 is Iter+1, 
+      ovbem(NIter, BatchSize, DataSet, Iter1, Eps))).
 
 ovbem1(DataSet, BatchSize, StepSize) :- 
     length(DataSet, N),
@@ -40,6 +46,43 @@ update_vb_params(StepSize, [A0|As0], [A1|As1], [AOut|AsOut]) :-
 
 multiply_datum_count(count(X, C), N, count(X, C1)) :- !, C1 is C * N.
 multiply_datum_count(X, N, count(X, N)).
+
+:- dynamic ovbem_alphas/3.
+initialize_ovbem_alphas :- retractall(ovbem_alphas).
+save_ovbem_alphas(Iter, Sw, Alpha) :- assert(ovbem_alphas(Iter, Sw, Alpha)).
+save_ovbem_alphas(Iter):- 
+    get_reg_sw_list(Sws), 
+    foreach(Sw in Sws, [Alpha, Sw, Vals],
+            (get_sw_a(Sw, [unfixed, Vals, Alpha]), 
+             save_ovbem_alphas(Iter, Sw, Alpha))).
+
+get_delta_ovbem_alphas(Iter, Sw, Delta) :- get_delta_ovbem_alphas(Iter, Sw, Delta, yes).
+get_delta_ovbem_alphas(Iter, Sw, Delta, IsNormed) :- 
+    Iter0 is Iter-1, 
+    ovbem_alphas(Iter, Sw, Alpha), 
+    ovbem_alphas(Iter0, Sw, Alpha0), 
+    l1_distance(Alpha, Alpha0, Delta1), 
+    (IsNormed = yes -> (length(Alpha, L), 
+                        Delta is Delta1/L); 
+                       Delta is Delta1).
+
+get_delta_ovbem_alphas(Iter, Delta) :- 
+    get_reg_sw_list(Sws), 
+    Deltas @= [D : Sw in Sws, [D],
+               get_delta_ovbem_alphas(Iter, Sw, D)],
+    Delta is sum(Deltas).
+
+    
+
+l1_distance([], [], 0) :- !.
+l1_distance([X|Xs], [Y|Ys], D) :- 
+    l1_distance(Xs, Ys, D1),
+    D is (abs(X-Y) + D1).
+l1_distance(_, _,_) :- throw(error('lists of different lengths', l1_distance)).
+
+    
+    
+                                       
                                               
     
     
--- src/GIJoe/SRS/Prismify.hs
+++ src/GIJoe/SRS/Prismify.hs
@@ -1,147 +1,112 @@
 module GIJoe.SRS.Prismify where 
 
+import Prelude hiding (head)
+
 import Data.Function (on)
-import Data.List
+import Data.List hiding (head)
 import Data.Text (unpack)
+
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 
 import GIJoe.SRS.Type
 import GIJoe.SRS.Parse
 
 
 
--- showPred :: Predicate -> String
--- showPred (Predicate p i) = concat ["'",unpack p,"_",show i,"'"]
-
 -- predTuplePair :: Predicate -> [String] -> String
 -- predTuplePair p xs = showPred p ++ "-" ++ showCleanList xs
 
--- showCleanList xs = "[" ++ (intercalate "," xs) ++ "]"
+type RuleMap = Map Predicate [Rule]
 
--- labeledMap f xs =  map f $ zip xs $ take (length xs) ['A'..'Z']
+mkRuleMap rs = Map.map (map dropRuleWeight) $ groupRulesByHeadPredicate rs
 
--- prismClauses :: RewriteSystem -> String
--- prismClauses rs = unlines $ 
---   switchClauses ++ [""] ++
---   probClauses ++ [""] ++
---   reductionClauses ++ [""] ++ 
---   prismToSRSClauses ++ [""] ++
---     [":- include(vis)."]
+mkValues pred ruleMap = [dynamic_dec, value_def, value_pred_def, lpn_value_pred_assert]
+  where switchName = showQuoted pred
+        value_pred = quoted $ show pred ++ "_values"
+        dynamic_dec = ":- dynamic " ++ value_pred ++ "/1."
+        value_def =  "values(" ++ switchName ++ ", Vs) :- " ++ value_pred ++ "(Vs)."
+        value_pred_def = value_pred ++"(" ++ show [1..numRules] ++ ")."
+        lpn_value_pred_assert = ":- assert_lpn_value_pred("
+                                ++ switchName ++ ", " ++ value_pred ++ ")."
+        rules = ruleMap ! pred
+        numRules = length rules
 
---   where switchClauses = map makeSwitch groupedRules
---         probClauses = map makeProbs groupedRules
---         reductionClauses = concatMap makeReductions groupedRules
---         groupedRules = groupRulesByHeadPredicate rs
---         prismToSRSClauses = concatMap makePrismToSRS groupedRules
+mkAllValues ruleMap = unlines [unlines $ mkValues p ruleMap | p <- Map.keys ruleMap]
 
-makeSwitch :: (Show r, IsRule r) => 
-              Predicate
-           -> [r]
-           -> String
-makeSwitch p rules = unlines [lexSwitchString, reduceSwitchString]
-  where
-    lexRules = filter isLexRule rules
-    reduceRules = filter (not . isLexRule) rules
-    lexSwitchString = makeSwitch' p lexRules True
-    reduceSwitchString = makeSwitch' p reduceRules False
+mkAllRules :: RuleMap -> String
+mkAllRules ruleMap = unlines [unlines $ mkPredRule p ruleMap | p <- Map.keys ruleMap]
 
-isLexRule :: IsRule r => r -> Bool
-isLexRule = null . body
+mkAllPrismToLPN :: RuleMap -> String
+mkAllPrismToLPN ruleMap = unlines $ do
+  p <- Map.keys ruleMap
+  return $ unlines $do
+    (i, r) <- zip [1..] $ ruleMap ! p
+    return $ mkPrismToLPN r i
 
-makeSwitch' :: (Show r, IsRule r) =>
-              Predicate
-              -> [r]
-              -> Bool -- ^ is lexical rule
-              -> String
-makeSwitch' p rules isLex = 
-  unlines $ [":- dynamic " ++ valuePred ++ "/1.",
-           "values(" ++ switchName ++ ", Vs) :- " ++ valuePred ++ "(Vs).",
-              valuePred ++"(" ++ show [1..k] ++ ")."] ++ [""] ++ toLPNs ++ probs
-  where valuePred = quoted $ show p ++ "_values" ++ if isLex then "_lex" else "_reduce"
-        k = length rules 
-        toLPNs = ["toLPN(" ++ switchName ++ ", " ++ show i ++ ", " ++ showQuoted rule ++ ")."
-                 | (i, rule) <- zip [1..] rules]
-        probs = [":- set_sw(" ++ switchName ++ ", " ++ show vs ++")"]
-                where vs = map (/ (sum ws)) ws
-                      ws = map weight rules
-        switchName | isLex = "lex(" ++ showQuoted p ++ ")"
-                   | otherwise = "reduce(" ++ showQuoted p ++ ")"
 
+mkPredRule :: Predicate -> RuleMap -> [String]
+mkPredRule pred ruleMap = do (i, r) <- zip [1..] rules
+                             return $ mkRule r i
+  where rules = ruleMap ! pred
+
+mkRule :: Rule -> Int -> String
+mkRule rule i =  _H ++ if null (_B1 ++ _C ++ _B2) then "."
+                         else (" :- \n\t\t" ++ intercalate ",\n\t\t" (filter (not.null) [_B1,  _C, _B2]) ++  ".")
+  where _A = quoted . show . headPredicate $ rule
+        ys = do (Argument els, k) <- zip (termArgs $ head rule) [(1::Int)..]
+                case els of
+                  [ElSym "null"]  -> return $ ("[]", Nothing)
+                  [ElSym e] -> return $ ("[" ++ quoted e ++ "]", Nothing)
+                  [ElVar v] -> return $ (v, Nothing)                  
+                  els ->
+                    let v = "Z_" ++ show k 
+                    in return $ (v, Just $ "append(" ++
+                                    intercalate ", " (map show els ++ [v]) ++ ")")
+                                    
+                                                 
+
+        ts = do Term p args <- body rule
+                let as = do Argument els <- args
+                            case els of
+                              [ElSym "null"]  -> return $ "[]"
+                              [ElSym e] -> return $ "[" ++ quoted e ++ "]"
+                              [ElVar v] -> return $ v
+                              _ -> error $ "multiple variables in lhs terms."
+                return $ "prove(" ++ showQuoted p
+                          ++ "-[" ++ (intercalate ", " as) ++ "])"
+                
+                                    
+        _H = "rule(" ++ _A ++ "-[" ++ intercalate ", " (map fst ys) ++ "], "
+             ++ show i ++ ")"
+        _B1 =  intercalate ", " $ ["(var(" ++ v ++ ") -> true; " ++ fromJust y ++ ")"| (v, y) <- ys, isJust y]
+        _B2 =  intercalate ", " $ ["(var(" ++ v ++ ") -> " ++ fromJust y ++ "; true)"| (v, y) <- ys, isJust y]
+        isJust (Just _) = True
+        isJust _ = False
+        fromJust (Just x) = x
+
+        _C = intercalate ", " $ ts
+                
+mkPrismToLPN :: Rule -> Int -> String
+mkPrismToLPN rule i = "prismToLPN(" ++ intercalate ", " args ++ ")."
+  where switch = showQuoted $ headPredicate rule
+        ruleString = showQuoted rule
+        args = [switch, show i, ruleString]
+
+
+prismClauses :: RewriteSystem -> String
+prismClauses sys = unlines [":- include('prove.psm').",
+                            mkAllValues ruleMap, "",
+                            mkAllRules ruleMap, "",
+                            mkAllPrismToLPN ruleMap]
+  where ruleMap = mkRuleMap sys        
+
+prismify :: FilePath -> FilePath -> IO ()
+prismify inpath outpath = do
+  sys <- readSystem inpath
+  writeFile outpath $ prismClauses sys
+
+
+------
 showQuoted x = "'" ++ show x ++ "'"
 quoted x = "'" ++ x ++ "'"
-
--- makeProbs :: [WeightedRule] -> String
--- makeProbs rs =
---     ":- set_sw(" ++ weightedRuleName (head rs) ++ "," ++ show normProbList ++ ")."
---   where
---     normProbList = map (/ (sum probList)) probList
---     probList = map pullProb rs
---     pullProb = (\r -> read (show $ weightedRuleWeight r) :: Double)
-
--- makeReductions :: [WeightedRule] -> [String]
--- makeReductions rs = map makeReduction indexedRules
---   where
---     indexedRules = zip [1..(length rs)] (map weightedRuleRule rs)
-
--- makeReduction :: (Int,Rule) -> String
--- makeReduction (i,(h :<-: bs)) =
---     "reduce(" ++ refactorHead h ++ "," ++ show i ++ ")" ++ body ++ "."
---   where
---     appendVars = collectTupleItems h
---     srsTerms = createSRSTerms h bs
---     appendAppends = createAppendTerms h
-    
---     rhsTerms | null appendVars = []
---              | otherwise = groundConditionals ++ [srsTerms] ++ varConditionals
---     arity = predArity . cTermPredicate $ h
---     groundConditionals = take arity ["(var(A2) -> true; append(X, Y, A2))",
---                                                    "(var(B2) -> true; append(U, V, B2) )"]
---     varConditionals = take arity ["(var(A2) -> append(X, Y, A2) ; true)",
---                                 "(var(B2) -> append(U, V, B2) ; true)"]
-                      
---     body | not (rhsTerms == []) = ":- " ++ intercalate ",\n" rhsTerms
---          | otherwise = ""
-
---     collectTupleItems (ComplexTerm _ xs) = intercalate ", " $ filter (not . null) $ labeledMap collectTupleItem xs
---     collectTupleItem ((NTString (x:[])),l) = ""
---     collectTupleItem ((NTString xs),l) = "var(" ++ l:(show $ length xs) ++ ")"
-
--- createSRSTerms (ComplexTerm _ xs) bs = if null termList then "" else intercalate ", " termList
---   where
---     termList = map sTermToPair bs
---     sTermToPair h = "srs(" ++ predTuplePair (sTermPredicate h) (map unpack (stermArgs h)) ++ ")"
-
--- refactorHead h@(ComplexTerm _ xs) = predTuplePair (cTermPredicate h) (renameTupleItems xs)
---   where
---     renameTupleItems xs = labeledMap renameTupleItem xs
---     renameTupleItem ((NTString (x:[])),l) = case x of
---       ElVar _ -> show x
---       ElSym y -> if (unpack y) == "null" then "[]" else show [x]
---     renameTupleItem ((NTString xs),l) = l:(show $ length xs)
-
-
--- createAppendTerms h@(ComplexTerm _ xs) =
---     intercalate ", " $ filter (not . null) $ labeledMap createAppends xs
---   where
---     createAppends :: (NonTerminalString,Char) -> String
---     createAppends ((NTString xs),l) =  intercalate ", " $
---       trd3 $ foldl (\(i,prev,acc) curr -> (i+1,
---                                            l:(show $ i+1),
---                                            (appendString prev i curr l):acc))
---         (1,show (xs !! 0),[]) $ drop 1 xs
---     appendString p i x l = concat ["append(",p,",",newVarName x,",",(l:(show $ i+1)),")"]
---     newVarName x = case x of
---       ElVar _ -> show x
---       ElSym _ -> "[" ++ show x ++ "]"
-
--- prismify :: FilePath -> FilePath -> IO ()
--- prismify inpath outpath = do
---   slcfrs <- readSystem inpath
---   writeFile outpath $ prismClauses slcfrs
-
-
--- -------------
--- -- Helpers
--- fst3 (a,_,_) = a
--- snd3 (_,b,_) = b
--- trd3 (_,_,c) = c
--- -------------
