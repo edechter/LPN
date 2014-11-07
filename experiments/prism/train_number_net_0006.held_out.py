5d475cc63ce3d3ca74715cd54c4cac9737eb6cfc
Eyal Dechter <eyaldechter@gmail.com>
2014-11-07 10:48:40 -0500

    Removed title from plot and bounded x axis.

--- experiments/prism/train_number_net_0006.held_out.py
+++ experiments/prism/train_number_net_0006.held_out.py
@@ -18,13 +18,14 @@ data = data[1:, 1]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(data, 
+ax.plot(range(1, len(data) + 1), 
+        data, 
         linewidth=2, 
         marker='o')
 ax.set_xlabel("Iteration")
 ax.set_ylabel(r"$\mathbb{E} \log p(D_{\text{test}})$")
-ax.set_title("Evaluating stochastic VBEM for LPNs")
 ax.set_ylim(top=1000)
+ax.set_xlim(right=20)
 
 # line at 0
 [lo, hi] = ax.get_xlim()
